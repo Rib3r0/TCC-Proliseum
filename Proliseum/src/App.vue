a1f0daf76379bae0d31fc9b402ec077cd331dbaf
Eduardo Ribeiro Pimenta <eduardorpimentaa@gmail.com>
2023-08-21 11:22:58 -0300

    inicializacao do projeto

---
+++ Proliseum/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Oi</h1>
+  <customised-button></customised-button>
+</template>
+
+<script>
+import CustomisedButton from './components/CustomisedButton.vue'
+  export default{
+    name: 'App',
+    components:{
+      CustomisedButton
+    }
+  }
+</script>
+       
---
+++ Proliseum/src/components/CustomisedButton.vue
@@ -0,0 +1,20 @@
+<template>
+<button type="button"><p>{{ text }}</p></button>
+</template>
+
+<script>
+export default {
+    name: "CustomisedButton",
+    data(){
+        return {
+            text: "ab"
+        }
+    }
+}
+</script>
+
+<style>
+button{
+    background-color: tomato;
+}
+</style>
