fd6bf86dfb0c8c95a8ca7885f8badad44b906b1c
Eduardo <eduardorpimentaa@gmail.com>
2023-12-06 10:11:20 -0300

    aa

--- Proliseum/src/axios/axios.js
+++ Proliseum/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosPerfil = axios.create({
-    baseURL: 'https://proliseum-back.cyclic.app/',
+    baseURL: 'https://alive-dog-parka.cyclic.app/',
     headers: {
         'Content-Type': "application/json",
         Authorization : `Bearer ${localStorage.getItem('token')}`,
